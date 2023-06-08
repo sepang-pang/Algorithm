07194da951419de3f1835e6fcb7bae93ede0ea43
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-06-08 15:36:56 +0900

    <frat> : 문자열 뒤집기 알고리즘

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,2 +1,25 @@
+import java.util.Stack;
+
 public class ReverseString {
+    public String solution(String my_string) {
+        Stack<Character> stack = new Stack<>();
+        char[] charItem = new char[my_string.length()];
+        String answer ="";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            stack.push(my_string.charAt(i));
+        }
+
+        for (int i = 0; i < my_string.length(); i++) {
+            charItem[i] = stack.pop();
+            answer = String.valueOf(charItem);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+      ReverseString reverseString = new ReverseString();
+        System.out.println(reverseString.solution("coffee"));
+
+    }
 }
