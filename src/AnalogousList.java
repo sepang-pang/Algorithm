f4b0a0f2501d8185830a80912909f92fc6228408
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-06-18 17:55:03 +0900

    <frat> : 두 배열 유사도 검사 => for 문으로 구현하는 방법 2 개 추가

--- src/AnalogousList.java
+++ src/AnalogousList.java
@@ -4,14 +4,30 @@ class AnalogousList {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
 
-        // 중복 제거를 위해 set 생성
-        Set<String> set1 = new HashSet<>(Arrays.asList(s1));
-        Set<String> set2 = new HashSet<>(Arrays.asList(s2));
+//        // 중복 제거를 위해 set 생성
+//        Set<String> set1 = new HashSet<>(Arrays.asList(s1));
+//        Set<String> set2 = new HashSet<>(Arrays.asList(s2));
+//
+//        // 두 Set 비교
+//        for (String setItem : set1) {
+//            if (set2.contains(setItem)) {
+//                answer ++;
+//            }
+//        }
 
-        // 두 Set 비교
-        for (String setItem : set1) {
-            if (set2.contains(setItem)) {
-                answer ++;
+//        for (int i = 0; i < s1.length; i++) {
+//            for (int j = 0; j < s2.length; j++) {
+//                if (s1[i].equals(s2[j])) {
+//                    answer++;
+//                }
+//            }
+//        }
+
+        for (String s1Item : s1) {
+            for (String s2Item : s2) {
+                if (s1Item.equals(s2Item)){
+                    answer ++;
+                }
             }
         }
 
