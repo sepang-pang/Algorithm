436180a5e30d58454e18c5841d13f31d7ba988b0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-27 03:38:52 +0900

    Time: 2 ms, Memory: 42.1 MB - LeetHub

---
+++ sort-colors/sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+       
+        for (int i = 0; i < nums.length-1; i++) {
+            int minIdx = i;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] < nums[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            
+            int temp = nums[minIdx];
+            nums[minIdx] = nums[i];
+            nums[i] = temp;
+        }
+        
+    }
+}
