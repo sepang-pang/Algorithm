f1291cb695f5dd25433645563750cca7232fe7ce
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-29 04:34:33 +0900

    Time: 19 ms (10.36%), Space: 44 MB (90.8%) - LeetHub

--- LeetCode/Easy/1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ LeetCode/Easy/1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,6 +1,36 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-         Set<Integer> integerSet = new HashSet<>();
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                int freqA = frequencyMap.get(nums[j]);
+                int freqB = frequencyMap.get(nums[j + 1]);
+
+                if (freqA > freqB || (freqA == freqB && nums[j] < nums[j + 1])) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+
+        return nums;
+    }
+}
+
+
+/*
+    try 1.
+    - 빈도수 계산: O(n^2)
+    - 버블 정렬: O(n^2)
+
+    class Solution {
+    public int[] frequencySort(int[] nums) {
+        Set<Integer> integerSet = new HashSet<>();
 
         for (int num : nums) {
             integerSet.add(num);
@@ -37,3 +67,4 @@ class Solution {
         return nums;
     }
 }
+*/
