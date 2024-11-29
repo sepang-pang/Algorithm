c9e29510f28438deaa5bf42775b67e36c98e38cf
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-29 18:29:41 +0900

    Time: 7 ms (42.35%), Space: 46.4 MB (80.86%) - LeetHub

--- LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,23 +1,43 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] result = new int[nums.length];
+       int[] result = new int[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
             result[i] = nums[i] * nums[i];
         }
 
-        for (int i = 0; i < result.length - 1; i++) {
-            for (int j = 0; j < result.length - 1 - i; j++) {
-                if (result[j + 1] < result[j]) {
-                    int swap = result[j + 1];
-                    result[j + 1] = result[j];
-                    result[j] = swap;
-                }
-            }
-        }
+        Arrays.sort(result);
+        
         return result;
     }
 }
+
+/*
+    Try 1.
+    RunTime : 437 ms
+    Memory : 47.6 mb
+    class Solution {
+        public int[] sortedSquares(int[] nums) {
+            int[] result = new int[nums.length];
+
+            for (int i = 0; i < nums.length; i++) {
+                result[i] = nums[i] * nums[i];
+            }
+
+            for (int i = 0; i < result.length - 1; i++) {
+                for (int j = 0; j < result.length - 1 - i; j++) {
+                    if (result[j + 1] < result[j]) {
+                        int swap = result[j + 1];
+                        result[j + 1] = result[j];
+                        result[j] = swap;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+*/
+
 
 /*
     Two-Pointer 알아보기
