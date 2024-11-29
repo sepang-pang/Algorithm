ad68a1c926c097a5b591760317f284e28f6a135c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-29 18:00:41 +0900

    Time: 1 ms (97.85%), Space: 43.5 MB (20.51%) - LeetHub

--- LeetCode/Easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ LeetCode/Easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -3,19 +3,39 @@ class Solution {
         int evenCount = 0;
 
         for (int num : nums) {
-            
-            int divCount = 0;
-            
-            while (num > 0) {
-                num /= 10;
-                divCount++;
-            }
-
-            if (divCount % 2 == 0) {
+            if ((int)(Math.log10(num) + 1) % 2 == 0) {
                 evenCount++;
             }
         }
-
+        
         return evenCount;
     }
 }
+
+/*
+        Try 1.
+        Runtime : 1 ms
+        Memory : 43.29 mb
+
+        class Solution {
+        public int findNumbers(int[] nums) {
+            int evenCount = 0;
+
+            for (int num : nums) {
+
+                int divCount = 0;
+
+                while (num > 0) {
+                    num /= 10;
+                    divCount++;
+                }
+
+                if (divCount % 2 == 0) {
+                    evenCount++;
+                }
+            }
+
+            return evenCount;
+        }
+    }
+*/
