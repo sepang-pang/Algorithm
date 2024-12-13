01f10d78b14213f11eb9bc971b65224fb2159093
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-12-13 11:23:40 +0900

    Time: 1 ms (97.44%), Space: 45.1 MB (5.89%) - LeetHub

--- LeetCode/Easy/1089-duplicate-zeros/1089-duplicate-zeros.java
+++ LeetCode/Easy/1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,17 +1,26 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
         int n = arr.length;
-        int i = 0;
+        int zeros = 0;
 
-        while (i < n - 1) {
-            if (arr[i] == 0) {
-                for (int j = n - 2; j > i; j--) {
-                    arr[j + 1] = arr[j];
-                }
-                arr[i + 1] = 0;
-                i++;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) zeros++;
+        }
+
+        int writeIdx = n + zeros - 1; 
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (writeIdx < n) {
+                arr[writeIdx] = arr[i];
             }
-            i++;
+            writeIdx--;
+
+            if (arr[i] == 0) {
+                if (writeIdx < n) {
+                    arr[writeIdx] = 0;
+                }
+                writeIdx--;
+            }
         }
     }
 }
